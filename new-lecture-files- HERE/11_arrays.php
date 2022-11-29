8898cb2c830e07709c888e55f89e1ac97aa77948
Patryk <patryk.r.nowak@gmail.com>
2022-11-29 11:21:28 +0000

    lectures

---
+++ new-lecture-files- HERE/11_arrays.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $numberList = array(267, 8765, 345, '5345', 345, '<h1>Hello</h1>');
+
+    $numberList2 = [267, 8765, 345, '5345', 345, '<h1>Hello</h1>'];
+
+    // print_r($numberList);
+
+    echo $numberList[5];
+
+    echo $numberList2[4];
+
+
+    ?>
+
+</body>
+
+</html>
