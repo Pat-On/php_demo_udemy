c97058c282d80de344e369639a8bb25108c5258e
Patryk <patryk.r.nowak@gmail.com>
2022-11-29 15:39:33 +0000

    form in php

--- new-lecture-files- HERE/34_form_external.php
+++ new-lecture-files- HERE/34_form_external.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+
 <body>
 
-<form action="form_process.php" method="post">
-    
-<input type="text" name="username" placeholder="Enter Username">
-<input type="password"  name="password" placeholder="Enter Password"><br>
-<input type="submit" name="submit">
-    
-    
-</form>
+    <form action="34_form_process.php" method="post">
+
+        <input type="text" name="username" placeholder="Enter Username">
+        <input type="password" name="password" placeholder="Enter Password"><br>
+        <input type="submit" name="submit">
+
+
+    </form>
 
 
 
 </body>
+
 </html>
