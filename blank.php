2609f570e5284ac4d2cd048bd17f38a2cc617efb
Patryk <patryk.r.nowak@gmail.com>
2022-11-29 09:38:30 +0000

    first commit

---
+++ blank.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+
+    </head>
+    <body>
+
+
+        <?php
+            $title = "Hello world from variable"
+        ?>
+
+
+            <h1><?php echo $title ?></h1>
+    </body>
+</html>
---
+++ comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+
+    </head>
+    <body>
+
+
+        <?php
+            // Comment single line
+
+            /*
+                ah :>
+            */
+            $title = "Hello world from variable"
+        ?>
+
+
+            <h1><?php echo $title ?></h1>
+    </body>
+</html>
