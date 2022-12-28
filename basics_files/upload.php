feeb221482603a0dd8d669d58dcb118dc3457304
Patryk <patryk.r.nowak@gmail.com>
2022-12-28 08:56:14 +0000

    using info about upload

--- basics_files/upload.php
+++ basics_files/upload.php
@@ -1,5 +1,31 @@
 <?php
+// https://www.php.net/manual/en/features.file-upload.errors.php
+$upload_errors = array(
+    UPLOAD_ERR_OK           => "There is no error, the file uploaded with success.",
+    UPLOAD_ERR_INI_SIZE     => "The uploaded file exceeds the upload_max_filesize directive in php.ini.",
+    UPLOAD_ERR_FORM_SIZE    => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.",
+    UPLOAD_ERR_PARTIAL      => "The uploaded file was only partially uploaded.",
+    UPLOAD_ERR_NO_FILE      => "No file was uploaded.",
+    UPLOAD_ERR_NO_TMP_DIR   => "Missing a temporary folder.",
+    UPLOAD_ERR_CANT_WRITE   => "Failed to write file to disk.",
+    UPLOAD_ERR_EXTENSION    => "A PHP extension stopped the file upload."
+);
+
+$the_message = "";
+if (isset($_POST["submit"])) {
+
     print_r($_FILES["file_upload"]);
+
+    $the_error = $_FILES['file_upload']["error"];
+
+    $the_message =  $upload_errors[$the_error];
+}
+
+
+
+
+
+
 
 ?>
 
@@ -15,6 +41,16 @@
 
 <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
+
+
+        <?php
+
+        if (!empty($upload_errors)) {
+            echo "<h2>" . $the_message . "</h2>";
+        }
+
+
+        ?>
 
         <input type="file" name="file_upload">
 
